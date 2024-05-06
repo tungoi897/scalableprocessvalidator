6d73dbd447a4d58af8ad3ff56834442f527d335d
tungoi897 <samiwrobelpun0suy@hotmail.com>
2024-05-06 20:38:33 +0700

    feat task

---
+++ index.js
@@ -0,0 +1,9 @@
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+  return maxProfit;
+}
