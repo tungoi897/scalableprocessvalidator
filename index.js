f561184ab174c8b82b7450c338a5a82bca517243
tungoi897 <samiwrobelpun0suy@hotmail.com>
2024-05-07 07:58:37 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-function maxProfit(prices) {
-  let minPrice = Infinity;
-  let maxProfit = 0;
-  for (const price of prices) {
-    minPrice = Math.min(minPrice, price);
-    maxProfit = Math.max(maxProfit, price - minPrice);
-  }
-  return maxProfit;
-}
+const compact = (arr) => arr.filter(Boolean);
