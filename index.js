672114b21a9d5cbf13aef8f9465615c30fc3f84e
tungoi897 <samiwrobelpun0suy@hotmail.com>
2024-05-08 10:27:44 +0700

    demo

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const compact = (arr) => arr.filter(Boolean);
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const count = Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - 97]++;
+    count[t.charCodeAt(i) - 97]--;
+  }
+  return count.every((c) => c === 0);
+}
