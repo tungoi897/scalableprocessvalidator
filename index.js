45ca9bce2f4418407f52583025ba63da61c66d32
tungoi897 <samiwrobelpun0suy@hotmail.com>
2024-05-09 07:13:09 +0700

    update commit

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function isAnagram(s, t) {
-  if (s.length !== t.length) return false;
-  const count = Array(26).fill(0);
-  for (let i = 0; i < s.length; i++) {
-    count[s.charCodeAt(i) - 97]++;
-    count[t.charCodeAt(i) - 97]--;
-  }
-  return count.every((c) => c === 0);
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
 }
