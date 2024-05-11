0fd1d9c575a01bad26ab165bab01d22579bb32ef
tungoi897 <samiwrobelpun0suy@hotmail.com>
2024-05-11 22:56:57 +0700

    update commit

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-function letterCombinations(digits) {
-  if (digits.length === 0) return [];
-  const map = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
-  };
-  const result = [];
-  backtrack("", digits);
-  return result;
-  function backtrack(combination, nextDigits) {
-    if (nextDigits.length === 0) result.push(combination);
-    else {
-      const digit = nextDigits.substring(0, 1);
-      const letters = map[digit];
-      for (const letter of letters) {
-        backtrack(combination + letter, nextDigits.substring(1));
-      }
+function longestValidParentheses(s) {
+  if (s.length === 0) return 0;
+  let max = 0;
+  const dp = new Array(s.length).fill(0);
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === ")") {
+      if (s[i - 1] === "(") dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+      else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] === "(")
+        dp[i] =
+          dp[i - 1] + (i - dp[i - 1] >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+      max = Math.max(max, dp[i]);
     }
   }
+  return max;
 }
